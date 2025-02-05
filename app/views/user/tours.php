851730ea30288f3562483149e76bb9e24190c812
STARRED787 <malithamalshan7@gmail.com>
2025-02-05 13:01:11 +0530

    PHP

--- app/views/user/tours.php
+++ app/views/user/tours.php
@@ -1,3 +1,15 @@
+<?php
+
+// Include the necessary middleware for authentication
+require_once __DIR__ . '/../../middleware/userauthMiddleware.php';
+
+use App\Middleware\userauthMiddleware;
+
+// Authenticate the user before accessing the admin dashboard
+userauthMiddleware::authenticate(['user']);  // Only allow 'admin' role to access the dashboard
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
