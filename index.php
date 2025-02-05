a09d43a695f8ef23600f95965bc314b1d283dbd9
STARRED787 <malithamalshan7@gmail.com>
2025-02-05 13:06:12 +0530

    PHP

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-welcome to travel
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jetwing Travels</title>
+</head>
+
+<body>
+    <h1> Welcome</h1>
+</body>
+
+</html>
